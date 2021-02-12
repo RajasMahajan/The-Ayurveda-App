1129d366ca560b984d77d24b8a5a4d70b56796dd
RajasMahajan <73475718+RajasMahajan@users.noreply.github.com>
2021-02-12 07:12:36 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+var coldButton;
+function setup() {
+  createCanvas(400, 600);
+}
+
+function draw() {
+  background("red");
+  fill("black");
+  textSize(18);
+  text("The Ayurveda home remedy App", 70, 30);
+  textSize(16);
+  text("hey do you are sick?", 100, 70);
+  drawSprites();
+}
