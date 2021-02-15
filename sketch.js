1bc30f2abe5189d92631ae12075587d1e9ccad51
RajasMahajan <73475718+RajasMahajan@users.noreply.github.com>
2021-02-15 18:59:23 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -3,84 +3,143 @@ var coldButton;
 var coughButton;
 var headikeButton;
 var homescreenButton;
+var coldimage;
+var coughimage;
+var headikeimage;
+var intro1;
+var intro1image;
+var coughremedy;
+var coughremedyimage;
 var homescreenimage;
+var coughteaimage;
+var coughimagesprite;
+var headikeremedy;
+var headikeremedyimage;
+var coldHomeRemedy;
+var coldHomeRemedyimage;
 var gameState = 1;
 var HomeScreen = 1;
 var SolutionCold = 2;
 var SolutionCough = 3;
 var SolutionHeadike = 4;
 function preload() {
+  intro1image = loadImage("intro1.png");
   homescreenimage = loadImage("home.png");
+  coldimage = loadImage("cold.png");
+  coughimage = loadImage("cough.png");
+  headikeremedyimage = loadImage("headikehomeremedy.png");
+  headikeimage = loadImage("headike.png");
+  coughteaimage = loadImage("honeytea.jpg");
+  coughremedyimage = loadImage("coughRemedy.png");
+  coldHomeRemedyimage = loadImage("coldhomeremedy.png");
 }
 function setup() {
-  createCanvas(380, 600);
-  title = createSprite(193, 20, 250, 30);
-  coldButton = createSprite(200, 220, 200, 30);
-  coughButton = createSprite(200, 300, 200, 30);
-  headikeButton = createSprite(200, 380, 200, 30);
+  createCanvas(500, 600);
+  title = createSprite(203, 20, 250, 30);
+  // text image/sprite
+  intro1 = createSprite(250, 120, 50, 50);
+  intro1.addImage(intro1image);
+  intro1.scale = 0.5;
+  intro1.visible = false;
+  coldHomeRemedy = createSprite(250, 300, 50, 50);
+  coldHomeRemedy.addImage(coldHomeRemedyimage);
+  coldHomeRemedy.scale = 0.7;
+  coughremedy = createSprite(250, 200);
+  coughremedy.addImage(coughremedyimage);
+  coughremedy.scale = 0.7;
+  headikeremedy = createSprite(250, 310, 10, 10);
+  headikeremedy.addImage(headikeremedyimage);
+  headikeremedy.scale = 0.8;
+  coughimagesprite = createSprite(250, 430);
+  coughimagesprite.addImage(coughteaimage);
+  coughimagesprite.scale = 0.6;
+  //sprites
+  coldButton = createSprite(230, 230, 200, 30);
+  coughButton = createSprite(230, 350, 200, 30);
+  headikeButton = createSprite(230, 480, 200, 30);
   //homescreen button
-  homescreenButton = createSprite(10, 10, 30, 30);
+  homescreenButton = createSprite(20, 20, 30, 30);
   homescreenButton.addImage(homescreenimage);
-  homescreenButton.scale = "0.1";
-  homescreenButton.visible = false;
+  homescreenButton.scale = 0.1;
   //hiding sprites
-  coldButton.shapeColor = "yellow";
+  coldButton.addImage(coldimage);
+  coldButton.scale = 0.3;
   coldButton.visible = false;
-  coughButton.shapeColor = "skyblue";
+  coughButton.addImage(coughimage);
+  coughButton.scale = 0.36;
   coughButton.visible = false;
-  headikeButton.shapeColor = "green";
+  headikeButton.addImage(headikeimage);
+  headikeButton.scale = 0.28;
   headikeButton.visible = false;
   title.visible = false;
 }
 
 function draw() {
-  background(138, 219, 208);
+  background(27, 235, 23);
   // gameState 1 (home screen)
   if (gameState === 1) {
+    coldHomeRemedy.visible = false;
+    coughremedy.visible = false;
+    homescreenButton.visible = false;
+    headikeremedy.visible = false;
+    coughimagesprite.visible = false;
+    intro1.visible = true;
     fill("black");
     title.shapeColor = "blue";
     textSize(18);
     textSize(16);
-    text("Hey,Are you sick?", 10, 70);
+    //  text("Hey,Are you sick?", 10, 70);
     title.visible = true;
     coldButton.visible = true;
     coughButton.visible = true;
     headikeButton.visible = true;
-    drawSprites();
-    text("Cold", 185, 225);
-    text("Cough", 180, 305);
-    text("Headike", 175, 385);
-    text("The Ayurveda Home Remedy App", 70, 30);
-    text("       Try this Here you can fing every disease Home", 10, 100);
-    text("Remedy Solution. You can select the disease which", 10, 120);
-    text("you have by clicking on it", 10, 135);
+    //
     if (mousePressedOver(coughButton)) {
       gameState = 3;
     }
   }
+  if (mousePressedOver(coldButton)) {
+    gameState = 2;
+  }
+  if (mousePressedOver(headikeButton)) {
+    gameState = 4;
+  }
   // gameState 2 (home remedy)
   if (gameState === 3) {
+    headikeremedy.visible = false;
+    intro1.visible = false;
+    coughButton.visible = false;
     coldButton.visible = false;
+    headikeButton.visible = false;
+    title.visible = false;
+    //   coldHomeRemedy.visible = true;
+    coughremedy.visible = true;
+    homescreenButton.visible = true;
+    coughimagesprite.visible = true;
+  }
+  if (gameState === 2) {
+    intro1.visible = false;
+    headikeremedy.visible = false;
     coughButton.visible = false;
     headikeButton.visible = false;
     title.visible = false;
-    fill("black");
-    text(
-      "If you have cough you need to take the following home Remedy's",
-      5,
-      50
-    );
-    text(
-      "1)Use honey to treat a cough, mix 2 teaspoons (tsp) with warm water",
-      5,
-      100
-    );
-    text("or an herbal tea. Drink this mixture once or twice a day.", 5, 120);
+    coldButton.visible = false;
     homescreenButton.visible = true;
-
-    if (mousePressedOver(homescreenButton)) {
-      gamesState = 1;
-    }
-    drawSprites();
+    coldHomeRemedy.visible = true;
   }
+  //gamestate=4
+  if (gameState === 4) {
+    intro1.visible = false;
+    headikeremedy.visible = false;
+    coughButton.visible = false;
+    headikeButton.visible = false;
+    title.visible = false;
+    coldButton.visible = false;
+    headikeremedy.visible = true;
+    homescreenButton.visible = true;
+  }
+  if (mousePressedOver(homescreenButton)) {
+    gameState = 1;
+  }
+  drawSprites();
 }
